6d1af6c75d75cd261d585fd338efa26f0ad54063
SamAlliger <sam.alliger@web.de>
2024-12-08 12:52:36 +0100

    Added Player, drawing to screen

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # throughout this file
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init
@@ -11,15 +12,27 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    # Initialize the screen
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    # Initialize clock
     clock = pygame.time.Clock()
     dt = 0
 
+    # Initialize player in the middle of the screen
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
+    # Infinte game loop
     while True:
+        # Quit when window is closed
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        # Black background
         screen.fill(color="black", rect=None, special_flags=0)
+        # Player character
+        player.draw(screen)
+        # Refresh screen, wait on clock
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
